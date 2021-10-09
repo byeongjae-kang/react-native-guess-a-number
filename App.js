b94fd18ac3cffecd16fff525089bfce9db75694d
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-10-09 16:34:44 -0700

    import and render StartGameScreen screen, and add a console.log() to see if app is running okay

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import React from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
+import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
+  console.log("App is running");
   return (
     <View style={styles.screen}>
       <Header title="Guess a Number" />
+      <StartGameScreen />
     </View>
   );
 }
