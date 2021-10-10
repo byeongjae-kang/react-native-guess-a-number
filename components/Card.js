a81688e2b57c46e17b9408d2a94a0869e7070bf3
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-10-09 17:00:56 -0700

    create and implement reusable card component

---
+++ components/Card.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { View, StyleSheet } from "react-native";
+
+function Card(props) {
+  return (
+    <View style={{ ...styles.card, ...props.style }}>{props.children}</View>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    backgroundColor: "white",
+    borderRadius: 12,
+    //for ios
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.26,
+    shadowRadius: 5,
+    //for android
+    elevation: 10
+  }
+});
+
+export default Card;
--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,18 +1,19 @@
 import React from "react";
 import { View, Text, StyleSheet, Button, TextInput } from "react-native";
+import Card from "../components/Card";
 
 function StartGameScreen(props) {
   return (
     <View style={styles.screen}>
       <Text style={styles.title}>Start a New Game</Text>
-      <View style={styles.inputContainer}>
+      <Card style={styles.inputContainer}>
         <Text>Select a Number</Text>
         <TextInput />
         <View style={styles.buttonContainer}>
           <Button title="Reset" />
           <Button title="Confirm" />
         </View>
-      </View>
+      </Card>
     </View>
   );
 }
@@ -31,15 +32,7 @@ const styles = StyleSheet.create({
     width: 300,
     maxWidth: "80%",
     alignItems: "center",
-    backgroundColor: "white",
-    padding: 20,
-    //for android
-    elevation: 10,
-    //for ios
-    shadowColor: "black",
-    shadowOffset: { width: 0, height: 2 },
-    shadowOpacity: 0.26,
-    shadowRadius: 5
+    padding: 20
   },
   buttonContainer: {
     flexDirection: "row",
