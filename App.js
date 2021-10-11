0c189100ad09eb9a0332986589813b26110ebe12
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-10-10 23:16:33 -0700

    load font family

--- App.js
+++ App.js
@@ -4,11 +4,30 @@ import Header from "./components/Header";
 import StartGameScreen from "./screens/StartGameScreen";
 import GameScreen from "./screens/GameScreen";
 import GameOverScreen from "./screens/GameOverScreen";
+import * as Font from "expo-font";
+import AppLoading from "expo-app-loading";
+
+const fetchFont = () => {
+  return Font.loadAsync({
+    "open-sans": require("./assets/fonts/OpenSans-Regular.ttf"),
+    "open-sans-bold": require("./assets/fonts/OpenSans-Bold.ttf")
+  });
+};
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
   const [guessRounds, setGuessRounds] = useState(0);
+  const [dataLoaded, setDataLoaded] = useState(false);
 
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFont}
+        onFinish={() => setDataLoaded(true)}
+        onError={() => console.log("error")}
+      />
+    );
+  }
   const newGameHandler = () => {
     setGuessRounds(0);
     setUserNumber(null);
