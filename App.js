0022c7f35678647bd5cc267e6b578e94bde4ebe7
Byeongjae Kang <bart.kang.0719@gmail.com>
2021-10-10 22:05:34 -0700

    import and render game over screen dynamically based on state

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
 import React, { useState } from "react";
 import { StyleSheet, View } from "react-native";
 import Header from "./components/Header";
-import GameScreen from "./screens/GameScreen";
 import StartGameScreen from "./screens/StartGameScreen";
+import GameScreen from "./screens/GameScreen";
+import GameOverScreen from "./screens/GameOverScreen";
 
 export default function App() {
   const [userNumber, setUserNumber] = useState();
+  const [guessRounds, setGuessRounds] = useState(0);
 
   const StartGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber);
+    setGuessRounds(0);
+  };
+
+  const gameOverHandler = (numberOfRounds) => {
+    setGuessRounds(numberOfRounds);
   };
 
   let screen = <StartGameScreen onGameStart={StartGameHandler} />;
 
-  if (userNumber) {
-    screen = <GameScreen userChoice={userNumber} />;
+  if (userNumber && guessRounds <= 0) {
+    screen = (
+      <GameScreen userChoice={userNumber} onGameOver={gameOverHandler} />
+    );
+  } else if (guessRounds > 0) {
+    screen = <GameOverScreen />;
   }
 
   return (
